ed925b1cd30a298d5817c6d1b49be9fdc6ac0d56
JonathanNoH <jonathan897h@gmail.com>
2022-03-16 14:25:06 -0600

    add testimonials

--- src/home.js
+++ src/home.js
@@ -22,7 +22,16 @@ const goHome = () => {
     //add testimonials
     const testimonials = document.createElement('div');
     testimonials.classList.add('testimonials');
-    testimonials.innerText = "Some of the best damn burgers I've ever had. My jelly was just squirting everywhere. -Jimmy (no relation)"
+
+    const addTestimonial = (string) => {
+        const testimonial = document.createElement('div');
+        testimonial.innerText = string;
+        testimonials.appendChild(testimonial);
+    }
+    addTestimonial("Some of the best damn burgers I've ever had. My jelly was just squirting everywhere. -Jimmy (no relation)");
+    addTestimonial("Well I guess it was fine. -Remy");
+    addTestimonial("I asked for a burger with no jelly and they kicked me out. I came back the next day though and got a burger with jelly. They were right to kick me out before -- I'll never have a burger without jelly again! -Anonymous");
+    addTestimonial("Better than the Krabby Patty -Gary");
 
     content.appendChild(testimonials);
 }
