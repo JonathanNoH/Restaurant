b09b27f6495f3feb63a1da7ef263b42d3d2acd11
JonathanNoH <jonathan897h@gmail.com>
2022-03-16 13:37:19 -0600

    add icon

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("Hello");
+import './style.css';
+import Icon from './icon.png';
+
+const content = document.querySelector('#content');
+
+//create header
+const header = document.createElement('header');
+
+//add icon
+const mainIcon = new Image();
+mainIcon.src = Icon;
+header.appendChild(mainIcon);
+
+//add title
+const mainTitle = document.createElement('h1');
+mainTitle.innerText = "Jim and Bob's Jellys and Burgers";
+header.appendChild(mainTitle);
+
+content.appendChild(header);
