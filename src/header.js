5cc7e58f55380db0e2a1c61b08b52766cfdf5a63
JonathanNoH <jonathan897h@gmail.com>
2022-03-16 14:54:12 -0600

    replace title with sidebar

--- src/header.js
+++ src/header.js
@@ -9,12 +9,26 @@ const createHeader = () => {
     //add icon
     const mainIcon = new Image();
     mainIcon.src = Icon;
+    mainIcon.alt = "Jim and Bob's Jellys and Burgers: Get some jelly in your burg";
     header.appendChild(mainIcon);
 
     //add title
-    const mainTitle = document.createElement('h1');
+    /*const mainTitle = document.createElement('h1');
     mainTitle.innerText = "Jim and Bob's Jellys and Burgers";
-    header.appendChild(mainTitle);
+    header.appendChild(mainTitle);*/
+
+    //add nav bar
+    const sidebar = document.createElement('div');
+    sidebar.classList.add('sidebar');
+    const addTab = (string) => {
+        const tab = document.createElement('div');
+        tab.innerText = string;
+        sidebar.appendChild(tab);
+    }
+    addTab('Home');
+    addTab('Menu');
+    addTab('Contact');
+    header.appendChild(sidebar);
 };
 
 export default createHeader;
