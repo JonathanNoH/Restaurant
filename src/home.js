970c7f44a0cf435e068c19512a53a6dd1b650e74
JonathanNoH <jonathan897h@gmail.com>
2022-03-16 13:53:33 -0600

    move homepage to own module

---
+++ src/home.js
@@ -0,0 +1,30 @@
+import './style.css';
+import Icon from './icon.png';
+
+const content = document.querySelector('#content');
+
+const goHome = () => {
+    //create header
+    const header = document.createElement('header');
+
+    //add icon
+    const mainIcon = new Image();
+    mainIcon.src = Icon;
+    header.appendChild(mainIcon);
+
+    //add title
+    const mainTitle = document.createElement('h1');
+    mainTitle.innerText = "Jim and Bob's Jellys and Burgers";
+    header.appendChild(mainTitle);
+
+    content.appendChild(header);
+
+    //add testimonials
+    const testimonials = document.createElement('div');
+    testimonials.classList.add('testimonials');
+    testimonials.innerText = "Some of the best damn burgers I've ever had. My jelly was just squirting everywhere. -Jimmy (no relation)"
+
+    content.appendChild(testimonials);
+}
+
+export default goHome;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,5 @@
 import './style.css';
 import Icon from './icon.png';
+import goHome from './home';
 
-const content = document.querySelector('#content');
-
-//create header
-const header = document.createElement('header');
-
-//add icon
-const mainIcon = new Image();
-mainIcon.src = Icon;
-header.appendChild(mainIcon);
-
-//add title
-const mainTitle = document.createElement('h1');
-mainTitle.innerText = "Jim and Bob's Jellys and Burgers";
-header.appendChild(mainTitle);
-
-content.appendChild(header);
-
-//add testimonials
-const testimonials = document.createElement('div');
-testimonials.classList.add('testimonials');
-testimonials.innerText = "Some of the best damn burgers I've ever had. My jelly was just squirting everywhere. -Jimmy (no relation)"
-
-content.appendChild(testimonials);
+goHome();
