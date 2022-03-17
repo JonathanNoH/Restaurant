c84624f2944f847e74512b8073a6310295255153
JonathanNoH <jonathan897h@gmail.com>
2022-03-17 12:33:20 -0600

    add font and hover style on links

--- src/index.js
+++ src/index.js
@@ -5,22 +5,22 @@ import goHome from './home';
 import goMenu from './menu';
 import goContact from './contact';
 
-
+//set up page
 createHeader();
 goHome();
-
+//grab Dom
 const content = document.querySelector("#content");
 const sidebar = document.querySelector(".sidebar");
 const home = document.querySelector(".home");
 const menu = document.querySelector(".menu");
 const contact = document.querySelector(".contact");
-
+//clear #content section
 const clear = () => {
     while (content.lastChild) {
         content.lastChild.remove();
     }
 }
-
+//set up buttons
 home.addEventListener('click', () => {
     clear();
     goHome();
