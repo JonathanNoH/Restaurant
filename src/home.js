2c51b00aae948ef5def12c71dbdc5a76f323b3de
JonathanNoH <jonathan897h@gmail.com>
2022-03-17 16:11:00 -0600

    update testimonials page styling and structure

--- src/home.js
+++ src/home.js
@@ -11,16 +11,27 @@ const goHome = () => {
     testimonialHeader.innerText = "Testimonials";
     testimonials.appendChild(testimonialHeader);
 
-    const addTestimonial = (string) => {
+    const addTestimonial = (text, author) => {
         const testimonial = document.createElement('div');
-        testimonial.innerText = string;
+        testimonial.classList.add('testimonial');
+
+        const testimonialBody = document.createElement('div');
+        testimonialBody.innerText = text;
+        testimonialBody.classList.add('testimonialBody');
+        testimonial.appendChild(testimonialBody);
+
+        const authorDiv = document.createElement('div');
+        authorDiv.innerText = author;
+        authorDiv.classList.add('testimonialAuthor');
+        testimonial.appendChild(authorDiv);
+
         testimonials.appendChild(testimonial);
     }
-    addTestimonial("Some of the best damn burgers I've ever had. My jelly was just squirting everywhere. -Jimmy (no relation)");
-    addTestimonial("Well I guess it was fine. -Remy");
-    addTestimonial("I asked for a burger with no jelly and they kicked me out. I came back the next day though and got a burger with jelly. They were right to kick me out before -- I'll never have a burger without jelly again! -Anonymous");
-    addTestimonial("Better than the Krabby Patty -Gary");
-    addTestimonial("I sure got some jelly in my burg! -Baron von Jellington")
+    addTestimonial("Some of the best damn burgers I've ever had. My jelly was just squirting everywhere.", "-Jimmy (no relation)");
+    addTestimonial("Well I guess it was fine.", "-Remy");
+    addTestimonial("I asked for a burger with no jelly and they kicked me out. I came back the next day though and got a burger with jelly. They were right to kick me out before -- I'll never have a burger without jelly again!", "-Anonymous");
+    addTestimonial("Better than the Krabby Patty!", "-Gary");
+    addTestimonial("I sure got some jelly in my burg!", "-Baron von Jellington");
 
     content.appendChild(testimonials);
 }
