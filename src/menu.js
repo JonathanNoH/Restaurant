34a7b60c1cb9f65e7849cb77b344e11239046248
JonathanNoH <jonathan897h@gmail.com>
2022-03-17 15:45:12 -0600

    add items. fix menu header not covering completely

--- src/menu.js
+++ src/menu.js
@@ -3,29 +3,29 @@ import './style.css';
 const content = document.querySelector('#content');
 
 const goMenu = () => {
-    //add top bar
+    //add consts
     const topBar = document.createElement('div');
     topBar.classList.add('menuHeader');
-    const addTopHeader = (string) => {
+    const menuSection = document.createElement('div');
+    menuSection.classList.add('menuArea');
+
+    const addNewSection = (string) => {
+        //add sections and top headers at once
+        //top header
         const topHeader = document.createElement('div');
         topHeader.innerText = string;
         topBar.appendChild(topHeader);
-        return topHeader;
-    }
-    const appetizersHeader = addTopHeader("Appetizers");
-    const burgersHeader = addTopHeader("Burgers");
-    const sidesHeader = addTopHeader("Sides");
-    const jelliesHeader = addTopHeader("Jellies and Desserts");
-
-    //add section headers
-    const menuSection = document.createElement('div');
-    menuSection.classList.add('menuArea');
-    const createSection = (string) => {
+        //section
         const section = document.createElement('div');
         section.classList.add('menuSection');
         addSection(string, section);
+        //add event listener
+        topHeader.addEventListener('click', () => {
+            section.scrollIntoView();
+        })
         return section;
     }
+
     const addSection = (string, section) => {
         const sectionHeader = document.createElement('div');
         sectionHeader.classList.add('menuSectionHeader');
@@ -34,22 +34,12 @@ const goMenu = () => {
         menuSection.appendChild(section);
     }
 
-    const appetizersSection = createSection("Appetizers");
-    appetizersHeader.addEventListener('click', () => {
-        appetizersSection.scrollIntoView();
-    })
-    const burgersSection = createSection("Burgers");
-    burgersHeader.addEventListener('click', () => {
-        burgersSection.scrollIntoView();
-    })
-    const sidesSection = createSection("Sides");
-    sidesHeader.addEventListener('click', () => {
-        sidesSection.scrollIntoView();
-    })
-    const jelliesSection = createSection("Jellies and Desserts");
-    jelliesHeader.addEventListener('click', () => {
-        jelliesSection.scrollIntoView();
-    })
+    //create sections
+    const appetizersSection = addNewSection("Appetizers");
+    const burgersSection = addNewSection("Burgers");
+    const sidesSection = addNewSection("Sides");
+    const dessertsSection = addNewSection("Desserts");
+    
     //add menu items
     const addMenuItem = (item, description, section) => {
         const menuItemDiv = document.createElement('div');
@@ -65,12 +55,16 @@ const goMenu = () => {
     }
     //appetizers
     addMenuItem('Jelly Tartare', 'Succulent raw jelly served in a small glass. Comes in red or blue flavour.', appetizersSection);
+    addMenuItem('Spinach Jelly', 'Our take on a spinach dip. Served with fresh breadsticks for dipping.', appetizersSection);
+    addMenuItem('Wings', 'Wings coated with any kind of jelly or dry rub with a ramekin of jelly on the side.', appetizersSection);
+    addMenuItem('Mozzarella Sticks', 'Mozzarella sticks with tomato jelly or tzatziki jelly', appetizersSection);
 
     //burgers
     addMenuItem('The Burg', 'A classic burger loaded with all the fixings plus a healthy serving of our seasonal jelly.', burgersSection);
     addMenuItem('The Baron Von Burgermeister', 'The regal burger: bacon, lettuce, tomato, grilled onions, garlic aioli, and truffle jelly.', burgersSection);
     addMenuItem('The BurgTown', 'A triple burger surrounded by a sampling of appetizers.', burgersSection);
     addMenuItem('The Mystery Burger', 'A mystery meat with a mystery jelly. Guess the jelly and you are entered in a monthly draw for a 200$ voucher!', burgersSection);
+    addMenuItem('The Peanutbutter and Jelly', 'A burger topped with grilled onions, gruyere, peanutbutter, and raspberry jelly.', burgersSection);
     addMenuItem('The Veg Burg', 'The classic burger with a veggie patty and no cheese', burgersSection);
 
     //sides
@@ -78,9 +72,9 @@ const goMenu = () => {
     addMenuItem('Onion Rings', 'Onion Rings with a choice of dip.', sidesSection);
 
     //desserts
-    addMenuItem('Berries & Cream', 'A creamy jelly infused with vanilla. Topped with fresh, seasonal berries.', jelliesSection);
-    addMenuItem('The Mystery Jelly', 'A mystery flavour jelly. Guess it and you are entered in a monthly draw for a 200$ voucher!', jelliesSection);
-    addMenuItem('The Iceburg', 'A slushie drink with your choice of jelly flavour. Topped with a mini burger.', jelliesSection);
+    addMenuItem('Berries & Cream', 'A creamy jelly infused with vanilla. Topped with fresh, seasonal berries.', dessertsSection);
+    addMenuItem('The Mystery Jelly', 'A mystery flavour jelly. Guess it and you are entered in a monthly draw for a 200$ voucher!', dessertsSection);
+    addMenuItem('The Iceburg', 'A slushie drink with your choice of jelly flavour. Topped with a mini burger.', dessertsSection);
 
     //append to content
     content.appendChild(topBar);
